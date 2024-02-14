1669a284819063e49758c298ae104833c9df5a7a
Bruno Alpachino <brunolalpachino@gmail.com>
2024-02-14 16:43:22 +0300

    styling of the column section

--- src/components/Columns.jsx
+++ src/components/Columns.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Columns = ({ title, headingColor, column, cards, setCards }) => {
+  // state to get the active card
+  const [active, setActive] = useState(false);
+
   return (
     <div className="w-56 shrink-0">
       {/* the shrink ensure the the contianer does not shrink to the view port */}
@@ -8,6 +11,12 @@ const Columns = ({ title, headingColor, column, cards, setCards }) => {
         <h3 className={`font-medium ${headingColor}`}>{title}</h3>
         <span className="rounded text-sm text-neutral-400">{cards.length}</span>
       </div>
+      {/* houses all the cards */}
+      <div
+        className={`h-full w-full transition-colors${
+          !active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
+      ></div>
     </div>
   );
 };
